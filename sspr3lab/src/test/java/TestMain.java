1ea2f8e8fc101102e1790d948f86d08b13aaa224
lelbyy <70452565+lelbyy@users.noreply.github.com>
2022-12-20 13:04:55 +0300

    asfadsfasdasf

--- sspr3lab/src/test/java/TestMain.java
+++ sspr3lab/src/test/java/TestMain.java
@@ -1,6 +1,14 @@
 import org.example.Main;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 
 public class TestMain {
 
@@ -8,4 +16,12 @@ public class TestMain {
     public void testInputIsEven(){
         assertEquals(Main.checkIfInputIsAnEvenNumber(21)); // Assertion
     }
+    @Test
+    @Disabled("asdasd")
+    void skippedTest() {
+    }
+    @Test
+    void failingTest(){
+        fail("failing")
+   }
 }
