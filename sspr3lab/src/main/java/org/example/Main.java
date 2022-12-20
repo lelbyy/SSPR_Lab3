df4aa7fe9573c56d2b4383723788851dcb3d8a3d
lelbyy <70452565+lelbyy@users.noreply.github.com>
2022-12-20 12:59:58 +0300

    Update Main.java

--- sspr3lab/src/main/java/org/example/Main.java
+++ sspr3lab/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(checkIfInputIsAnEvenNumber(123));
+        System.out.println(checkIfInputIsAnEvenNumber(122));
     }
 
     public static boolean checkIfInputIsAnEvenNumber(int number){
