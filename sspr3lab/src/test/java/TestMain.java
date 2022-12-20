623a0568b3a3044f5576e628b668501d245b19d9
lelbyy <70452565+lelbyy@users.noreply.github.com>
2022-12-20 11:56:52 +0300

    Update TestMain.java

--- sspr3lab/src/test/java/TestMain.java
+++ sspr3lab/src/test/java/TestMain.java
@@ -6,6 +6,6 @@ public class TestMain {
 
     @Test
     public void testInputIsEven(){
-        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+        assertTrue(Main.checkIfInputIsAnEvenNumber(2222)); // Assertion
     }
 }
